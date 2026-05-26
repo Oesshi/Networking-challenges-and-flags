d15c122af0e0fd71a7714abe6d42dd51bcf921a7
Oesshi LAHIRI <0041345@schoolsnet.act.edu.au>
2026-05-26 20:40:41 +1000

    Update bowlingpinchallengeintro.sh

--- bowlingpinchallengeintro.sh
+++ bowlingpinchallengeintro.sh
@@ -1,9 +1,21 @@
 # link to ctfd 
+ctfd_url=https://cbrc.ctfd.io/  
+challenge_name="bowlingpinchallengeintro.sh"
 
+score_mode="team"
+individual_points=75
+team_points=300
 
-
+if ["$score_mode" = "team" ]; then 
+   total_points=$team_points
+else
+    total_points=$individual_points
+fi
 # generate a flag using md5sum 
+flag_hash=$(echo -n "Strike_spare_secret_flag" | md5sum | awk '{print $1}')
+final_flag="flag{$flag_hash}"
 
+hidden_file_name=".bowlingpinflag.txt"
 
 #increase/ decreae points 
 
