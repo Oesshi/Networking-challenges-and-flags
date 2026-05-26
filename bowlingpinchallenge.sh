4f3fa576b2a2cbd7bdbcdbea513ffe89e998cd64
Oesshi LAHIRI <0041345@schoolsnet.act.edu.au>
2026-05-26 10:46:41 +1000

    Update bowlingpinchallenge.sh

--- bowlingpinchallenge.sh
+++ bowlingpinchallenge.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-ctfd_url="https://cbrc.ctfd.io"
-team_token="Player-unique-token"
-initial_points=200
-min_points=10
-pin_value=50
+ctfd_url="https://cbrc.ctfd.io/"
+total_pins=10
+individual_log="individual_pins.txt"
+team_log="team_pins.txt"
 
-  #Tracks the progress of fallen pins 
+calculate_progress() {
+   local log_file=$1
+   if [-f "$log_file" ]; then 
+   Fallen=$(cat "$log_file' | wc -l | tr -d '[:space:]')
+   Percent=$(echo "scale=2; ($Fallen / total_pins) * 100" | bc)
+  echo "fallen: $Fallen / $total_pins ($Percent%)"
+else
+   echo "Fallen : 0 / $total_pins($Percent%)
+fi
+
+}
+
+if [$1 == "knock"]; then 
+   echo"$(date): pin fell" >> "$2"
+   echo "Logged fallen pin to S2"
+fi
+
+echo "Individual Progress"
+calculate_progress "$individual_pins"
+
+echo "Team Progress"
+calculate_progress "$team_pins"    #Tracking the progress of fallen pins and ones that are still standing 
 
 # Generating a md5sum flag 
 flag_input = challenge_flag_text
@@ -15,9 +35,17 @@ echo "The flag you generated is : md5_flag"
 
 
 # Increasing/decrease points on ctfd 
+ctfd_url="https://cbrc.ctfd.io/"
+challenge_id="<your_challenge_id">
+api_token="${ctfd_token}"
 
+adjustment_value=50
 
-
+current_value=$(curl -s -x Get"${ctfd_id}/challenges/${challenge_id}"
+    -H "Authorisation: token ${api_token}"\
+    -H "Content_type : application/json" \
+    -d "{\value\" : ${New_value}}"
+echo "Successfully updated challenge ${challenge_id} from ${current_value} to ${new_value} points"
 
 #Linux commands 
 #!/bin/bash
